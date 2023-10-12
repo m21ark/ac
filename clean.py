31c5937c662200690a285d8b98872fbd67f68e0d
Ricardo Matos <ricardo.andre.de.matos@gmail.com>
2023-10-12 15:15:15 +0100

    adding accuracy to the pipeline

--- clean.py
+++ clean.py
@@ -3,6 +3,8 @@ import numpy as np
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.metrics import mean_squared_error
 from sklearn.preprocessing import StandardScaler
+from colorama import Fore, Style
+
 
 
 def pipeline_year(year = 10):
@@ -36,9 +38,12 @@ def pipeline_year(year = 10):
 
     #print(df_players_teams, df_teams)
     # call classification method
-    df_teams = classify_playoff_entry(df_players_teams, df_teams, year)
+    df_teams, ea_teams, we_teams = classify_playoff_entry(df_players_teams, df_teams, year)
 
-    # call evaluation function
+    ea_predictions = ea_teams['tmID'].unique()
+    we_predictions = we_teams['tmID'].unique()
+
+    calculate_playoff_accuracy(year, ea_predictions, we_predictions)
 
     return df_teams
 
@@ -63,12 +68,85 @@ def classify_playoff_entry(df_players_teams, df_teams, year):
     print(we_playoff_teams)
 
     for index, row in ea_playoff_teams.iterrows():
-        row['tmID'] = 'Y'
+        row['playoff'] = 'Y'
 
     for index, row in we_playoff_teams.iterrows():
-        row['tmID'] = 'Y'
+        row['playoff'] = 'Y'
 
-    return df_teams
+    return df_teams, ea_playoff_teams, we_playoff_teams
+
+
+def color_text(predicted, actual):
+    colored_text = []
+    for team in predicted:
+        if team in actual:
+            colored_text.append(Fore.GREEN + team + Style.RESET_ALL)
+        else:
+            colored_text.append(Fore.RED + team + Style.RESET_ALL)
+
+    return ', '.join(colored_text)
+
+
+def calculate_playoff_accuracy(year, predicted_ea_playoffs, predicted_we_playoffs):
+
+    results_df = pandas.read_csv("dataset/results.csv")
+
+    year_df_we = results_df[(results_df['year'] == year)
+                            & (results_df['confID'] == 'WE')]
+    year_df_ea = results_df[(results_df['year'] == year)
+                            & (results_df['confID'] == 'EA')]
+
+    actual_we_playoffs = year_df_we[year_df_we['playoff']
+                                    == 'Y']['tmID'].tolist()
+    actual_ea_playoffs = year_df_ea[year_df_ea['playoff']
+                                    == 'Y']['tmID'].tolist()
+
+    all_we_teams = year_df_we['tmID'].tolist()
+    all_ea_teams = year_df_ea['tmID'].tolist()
+
+    team_conf_dict = {}
+
+    for team in all_we_teams:
+        team_conf_dict[team] = 'WE'
+    for team in all_ea_teams:
+        team_conf_dict[team] = 'EA'
+
+    we_incorrect = []
+    ea_incorrect = []
+    we_correct = []
+    ea_correct = []
+
+    sorted_team_list = list(predicted_ea_playoffs) + list(predicted_we_playoffs)
+
+    for team in sorted_team_list:
+        if team_conf_dict[team] == 'WE':
+            if team in actual_we_playoffs:
+                we_correct.append(team)
+            else:
+                we_incorrect.append(team)
+        else:
+            if team in actual_ea_playoffs:
+                ea_correct.append(team)
+            else:
+                ea_incorrect.append(team)
+
+    we_correct_count = len(we_correct)
+    ea_correct_count = len(ea_correct)
+
+    total_accuracy = ((we_correct_count + ea_correct_count) / 8.0) * 100
+
+    print("=" * 40)
+    print(f"{'Year:':<20}{year}")
+    print("=" * 40)
+    print("WE")
+    print(f"{'Guesses:':<15}{color_text(we_correct, actual_we_playoffs) + ', ' + color_text(we_incorrect, actual_we_playoffs) }")
+    print(f"{'Missed:':<15}{', '.join(set(actual_we_playoffs) - set(we_correct))}")
+    print("=" * 40)
+    print("EA")
+    print(f"{'Guesses:':<15}{color_text(ea_correct, actual_ea_playoffs) + ', ' + color_text(ea_incorrect, actual_ea_playoffs)}")
+    print(f"{'Missed:':<15}{', '.join(set(actual_ea_playoffs) - set(ea_correct))}")
+    print("=" * 40)
+    print(f"{'Total accuracy:':<20}{total_accuracy:.2f}%")
 
 
 def merge_player_info(df_players, df_players_teams):
@@ -312,9 +390,6 @@ def clean_players(df_players):
 
 
     return df_players
-
-
-    
 
 
 def remove_redundant_cols(df):
